3c5932dfdb44b0900133bdd1a3c186ab3bee28f4
Martin Nycander <martin.nycander@findwise.com>
2013-04-07 12:23:28 +0200

    Removes oh-my-zsh in preparation of moving it.

--- init.sh
+++ init.sh
@@ -2,14 +2,27 @@
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # The dir of this file
 
-# TODO: Update all submodules
+echo "Updating all git submodules"
+echo "---------------------------"
+git submodule init
+git submodule update
+echo ""
 
-# Install all dot files
+echo "Installing all dotfiles"
+echo "-----------------------"
 for dotfile in $ROOT/files/*
 do
 	if [ -f "$dotfile" ]; then
 		ln -sfv "$dotfile" ~/.$(basename "$dotfile")
 	fi
 done
+echo ""
 
-# TODO: Install all directories
+echo "Installing all dot directories"
+echo "------------------------------"
+for dotdir in $ROOT/dirs/*
+do
+	if [ -d "$dotdir" ]; then
+		ln -sfv "$dotdir" ~/.$(basename "$dotdir")
+	fi
+done
