7a0f57895a306d997dd70aba86f9547fb6d87486
Martin Nycander <martin.nycander@findwise.com>
2014-10-03 10:23:06 +0200

    More gitingnores, fix for incorrect hostnames. Beginnings of script to install everything from scratch on a mac

---
+++ install-mac.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+brew update
+
+brew install wget
+brew install watch 
+brew install git
+
+# Convert windows newlines to unix newlines
+brew install dos2unix
+
+# Mosh, ssh for bad connections
+brew install mobile-shell
